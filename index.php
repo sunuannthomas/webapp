20dcc048e3d41a7910c3fb4b1a72b76cac0af893
sunuannthomas <104191439+sunuannthomas@users.noreply.github.com>
2022-04-22 13:08:24 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>my web app</title>
+</head>
+<body>
+<h1>welcome to my web application</h1>
+<?php
+echo "<h1>TS FDP AT ASP Kerala</h1>";
+?>
+</body>
+</html>
